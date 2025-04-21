37c5512cf4f1b0ff561187aa4b9476766f4edba7
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2025-04-21 19:40:30 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function loadPage(page) {
+	fetch('../html/' + page)
+		.then(response => response.text())
+		.then(html => {
+			document.getElementById('page-content').innerHTML = html;
+		})
+		.catch(error => {
+			document.getElementById('page-content').innerHTML = '<p>Erro ao carregar a página.</p>';
+		});
+}
+
+window.onload = () => loadPage('../html/home.html');
